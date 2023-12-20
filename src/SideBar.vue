dde39ce46452f995232a949fff6af6cb60516eb4
Magnus <iwarrior.xr@gmail.com>
2023-12-20 22:38:46 +0800

    toast

--- src/SideBar.vue
+++ src/SideBar.vue
@@ -1,9 +1,10 @@
 <script setup>
 
 import {ref} from "vue";
+import {schema,host} from "../staticConfig.js"
 
 let showSideBar=ref(true)
-let toggleValue=ref('+')
+let toggleValue=ref('x')
 function clickToggle(){
     showSideBar=!showSideBar
     if (showSideBar){
@@ -11,7 +12,9 @@ function clickToggle(){
     }else{
         toggleValue.value='+'
     }
-
+}
+function toAbout(){
+    window.location.href=schema+"://"+host+"/about"
 }
 </script>
 
@@ -21,6 +24,7 @@ function clickToggle(){
         <div v-if="showSideBar" class="SideBarContent">
             <h1>共享小本本</h1>
             <h3>记录你遇到的坑，借鉴前人踩过的坑</h3>
+            <button @click="toAbout">详细介绍</button>
             <button id="setDefaultStart" onclick="setDefaultStart()">当前视野作为你的初始视野</button>
         </div>
     </div>
@@ -32,12 +36,15 @@ function clickToggle(){
 .SideBarWrapper{
     position: relative;
     background: black;
+
 }
 .ToggleSideBarBtn{
     pointer-events: auto;
-    font-size: xx-large;
+    font-size: xxx-large;
     color: yellow;
     background: black;
+    border:0;
+    margin: 0;
 
 }
 .SideBarContent{
